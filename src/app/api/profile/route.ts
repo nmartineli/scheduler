53398d7c583900ec8f9c57ff3b2a62dea21f634a
Natalia Martineli <nataliamartineli@gmail.com>
2023-12-08 11:59:32 -0300

    Updated mock and apis routes

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,16 +1,18 @@
-import { NextResponse} from "next/server";
+import { NextResponse } from 'next/server';
 import fsPromises from 'fs/promises';
 import path from 'path';
 
 const profileFilePath = path.join(process.cwd(), 'public/mocks/profile.json');
 
 export async function GET() {
-  try {
-    const profile = await fsPromises.readFile(profileFilePath, 'utf-8');
-    const json = JSON.parse(profile);
-    return NextResponse.json(json);
-  } catch (err) { 
-    return new NextResponse(JSON.stringify({ message: "No dishes found!" }),
-            { status: 404, headers: { 'content-type': 'application/json' } });  
-  }
+	try {
+		const profile = await fsPromises.readFile(profileFilePath, 'utf-8');
+		const json = JSON.parse(profile);
+		return NextResponse.json(json);
+	} catch (err) {
+		return new NextResponse(JSON.stringify({ message: 'No dishes found!' }), {
+			status: 404,
+			headers: { 'content-type': 'application/json' },
+		});
+	}
 }
