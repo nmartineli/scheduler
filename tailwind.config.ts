0b7907268932bc31b725b3390c6a966f57bd425e
Natalia Martineli <nataliamartineli@gmail.com>
2023-12-11 19:57:54 -0300

    Added image files and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					darkest: '#242F66',
+					dark: '#505985',
+					medium: '#7C82A3',
+					light: '#A7ACC2',
+					lightest: '#D3D5e0',
+				},
+				grayscale: {
+					black: '#000000',
+					dark: '#505050',
+					medium: '#767676',
+					light: '#c5c5c5',
+					white: '#FFFFFF',
+				},
+			},
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
